6b686f6e9907360d5d4fd0ce7cd7a6108a0c8966
Pixalesce <pixalesce@gmail.com>
2024-02-04 23:34:18 +0800

    feat: implemented connection between api and database containers
    end points created:
    - get all
    - create
    - get by id

--- backend/database/setup.sql
+++ backend/database/setup.sql
@@ -10,7 +10,7 @@ USE twitter_app;
 
 -- Create users table
 CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id CHAR(36) PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL
@@ -18,8 +18,8 @@ CREATE TABLE users (
 
 -- Create tweets table
 CREATE TABLE tweets (
-    tweet_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
+    tweet_id CHAR(36) PRIMARY KEY,
+    user_id CHAR(36),
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
@@ -28,19 +28,22 @@ CREATE TABLE tweets (
 -- Seed the database with fake data
 -- Insert users
 INSERT INTO
-    users (username, email, password_hash)
+    users (user_id, username, email, password_hash)
 VALUES
     (
+        '88cd187b-ce48-42d1-8696-2a8c756861b4',
         'user1',
         'user1@example.com',
         'hashed_password_1'
     ),
     (
+        'beebba51-d600-4927-a908-a2af174982cd',
         'user2',
         'user2@example.com',
         'hashed_password_2'
     ),
     (
+        '9d84387d-8f38-4b1b-b91a-e39e50cbd1c8',
         'user3',
         'user3@example.com',
         'hashed_password_3'
@@ -48,11 +51,11 @@ VALUES
 
 -- Insert tweets
 INSERT INTO
-    tweets (user_id, content)
+    tweets (tweet_id, user_id, content)
 VALUES
-    (1, 'This is a tweet from user1.'),
-    (2, 'Tweeting like user2.'),
-    (3, 'User3 joining the conversation.');
+    ('bdfd727d-6a9f-4229-9200-3324473eb805', '88cd187b-ce48-42d1-8696-2a8c756861b4', 'This is a tweet from user1.'),
+    ('e19ec413-fe1d-43b8-a964-238f3cbce6cc', 'beebba51-d600-4927-a908-a2af174982cd', 'Tweeting like user2.'),
+    ('2b235a33-dcb9-4d99-b5e3-7f0260083b92', '9d84387d-8f38-4b1b-b91a-e39e50cbd1c8', 'User3 joining the conversation.');
 
 -- Add more data as needed
 -- Display success message
