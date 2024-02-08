5cbb2126b7c70eac535f8f73b10100dd012e5e65
Joen Tan <joentze01@gmail.com>
2024-02-08 21:06:36 +0800

    feat: added user table

--- backend/database/setup.sql
+++ backend/database/setup.sql
@@ -9,52 +9,54 @@ CREATE DATABASE twitter_app;
 USE twitter_app;
 
 -- Create users table
-CREATE TABLE users (
-    uid INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE USERS (
+    uid VARCHAR(100) PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    is_private BIT DEFAULT 0 NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
--- Create tweets table
-CREATE TABLE tweets (
-    tweet_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
 
--- Seed the database with fake data
--- Insert users
-INSERT INTO
-    users (username, email, password_hash)
-VALUES
-    (
-        'user1',
-        'user1@example.com',
-        'hashed_password_1'
-    ),
-    (
-        'user2',
-        'user2@example.com',
-        'hashed_password_2'
-    ),
-    (
-        'user3',
-        'user3@example.com',
-        'hashed_password_3'
-    );
+-- -- Create tweets table
+-- CREATE TABLE tweets (
+--     tweet_id INT PRIMARY KEY AUTO_INCREMENT,
+--     user_id INT,
+--     content TEXT NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (user_id) REFERENCES users(user_id)
+-- );
 
--- Insert tweets
-INSERT INTO
-    tweets (user_id, content)
-VALUES
-    (1, 'This is a tweet from user1.'),
-    (2, 'Tweeting like user2.'),
-    (3, 'User3 joining the conversation.');
+-- -- Seed the database with fake data
+-- -- Insert users
+-- INSERT INTO
+--     users (username, email, password_hash)
+-- VALUES
+--     (
+--         'user1',
+--         'user1@example.com',
+--         'hashed_password_1'
+--     ),
+--     (
+--         'user2',
+--         'user2@example.com',
+--         'hashed_password_2'
+--     ),
+--     (
+--         'user3',
+--         'user3@example.com',
+--         'hashed_password_3'
+--     );
 
--- Add more data as needed
--- Display success message
-SELECT
-    'Database created and seeded successfully!' AS message;
+-- -- Insert tweets
+-- INSERT INTO
+--     tweets (user_id, content)
+-- VALUES
+--     (1, 'This is a tweet from user1.'),
+--     (2, 'Tweeting like user2.'),
+--     (3, 'User3 joining the conversation.');
+
+-- -- Add more data as needed
+-- -- Display success message
+-- SELECT
+--     'Database created and seeded successfully!' AS message;
