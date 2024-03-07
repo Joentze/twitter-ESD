23ad0ed17ae1b850bd4fb17aaba8939af909ad99
Joen Tan <joentze01@gmail.com>
2024-03-07 15:59:38 +0800

    feat: added publishing to email queue

--- backend/apis/complex/upload_post/upload_post.py
+++ backend/apis/complex/upload_post/upload_post.py
@@ -1,5 +1,6 @@
 """complex microservice for uploading post"""
 import os
+import json
 import pika
 from requests import get
 from typing import TypedDict, List
@@ -17,9 +18,8 @@ try:
 except KeyError:
     RABBITMQ_HOST = "localhost"
     RABBITMQ_PORT = 5672
-
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT))
+    pika.ConnectionParameters(host="localhost", port=RABBITMQ_PORT, heartbeat=3600, blocked_connection_timeout=3600))
 
 
 class UploadBody(TypedDict):
@@ -29,7 +29,7 @@ class UploadBody(TypedDict):
     post_images: List[str]
 
 
-@app.route("/upload/<string:uid>", method=["POST"])
+@app.route("/upload/<string:uid>", methods=["POST"])
 def upload_post(uid: str) -> None:
     """complex microservice to upload post"""
     try:
@@ -41,8 +41,9 @@ def upload_post(uid: str) -> None:
                 pass
             else:
                 email, username = get_user_email_name(uid)
+                print(email, username)
                 send_content_warning(email, username)
-
+            return jsonify({"code": 201, "message": "Successfuly uploaded post, we'll do a quick check to see if your post violated guidelines"}), 201
         except Exception as e:
             print(e)
             return jsonify({"code": 500, "message": "there was an error on the server"}), 500
@@ -58,7 +59,13 @@ def check_content(text: str) -> bool:
 
 def send_content_warning(email: str, username: str) -> None:
     """publishes content warning message to rabbitmq"""
-    return
+
+    channel = connection.channel()
+    channel.queue_declare(queue="notification-queue", durable=True)
+    message_body: str = json.dumps(
+        {"message_type": "CONTENT_WARNING", "email": email, "username": username})
+    channel.basic_publish(exchange="twitter-message-exchange",
+                          routing_key="email.notification", body=message_body)
 
 
 def get_user_email_name(uid: str) -> tuple:
@@ -67,3 +74,7 @@ def get_user_email_name(uid: str) -> tuple:
     response = get(user_info_route, timeout=5000)
     data = response.json()["data"]
     return data["user email"], data["username"]
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5122, debug=True)
